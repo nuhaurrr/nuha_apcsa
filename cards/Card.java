7651bedd4b9cefc1ecebc9c3793b64e2db202252
Nuha Shaikh <nuha8373@gmail.com>
2025-11-14 22:11:40 +0000

    completed card class

--- cards/Card.java
+++ cards/Card.java
@@ -1,6 +1,30 @@
 package cards;
 
 public class Card {
+    private int suit;
+    private int value;
+    private String[] SUIT {"♦", "♣", "♥", "♠"}
+    private String[] VALUE {"A","2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
+
+    public Card(int suit, int value){
+
+        this.suit = suit;
+        this.value = value;
+
+    }
+    public getValue(){
+        
+        return value;
+
+    }
+    public String toString(){
+
+        return "" + SUIT[suit] + VALUE[value];
+
+
+
+    }
+
 
 
 }
