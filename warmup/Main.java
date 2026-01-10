086c0a68cdcd439a493c661aa09fad129d637bdd
Nuha Shaikh <nuha8373@gmail.com>
2026-01-10 01:11:50 +0000

    added my varaibles and first method

--- warmup/Main.java
+++ warmup/Main.java
@@ -4,8 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
         // Your code here.
+        long number = 600851475143L;
+        long largestPrimeFactor = 0;
+
+        // Remove factors of 2
+        while (number % 2 == 0) {
+            largestPrimeFactor = 2;
+            number /= 2;
+        }
 
     }
 }
 
 //I am going to start the warm-up project
+//I have chosen to do problem 3
