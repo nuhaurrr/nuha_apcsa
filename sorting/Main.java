d3ac06949cdfb0bf49b4d8a79424f2c7b72fc721
Nuha Shaikh <nuha8373@gmail.com>
2026-02-04 03:43:48 +0000

    added stuff to the main and did insertion sort

--- sorting/Main.java
+++ sorting/Main.java
@@ -23,6 +23,17 @@ public class Main {
     public static int[] randomArray(int length)
     {
         // TODO: make this return an actual random array of the provided length.
+        public static int[] randomArray(int length)
+    {
+    int[] a = new int[length];
+
+    for (int i = 0; i < length; i++) {
+        a[i] = (int)(Math.random() * 100); // random numbers 0–99
+    }
+
+   
+    }
+
         int[] a = {10, 5, 3, 2};
         return a;
     }
