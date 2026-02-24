96f755af15a8ae21578faeab71d036530876e899
Nuha Shaikh <nuha8373@gmail.com>
2026-02-24 21:38:00 +0000

    started on methods

--- polygon/IrregularPolygon.java
+++ polygon/IrregularPolygon.java
@@ -14,6 +14,7 @@ public class IrregularPolygon {
     public void add(Point2D.Double aPoint)
     {
         // TODO: Add a point to the IrregularPolygon.
+        
     }
 
     public double perimeter() {
